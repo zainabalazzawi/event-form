c8ae03b3073988d5b69987d42a089f777961d574
Zainab Ahmad <zainab.ahmad@stadim.be>
2024-10-05 13:42:05 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  firstName: z.string().min(2, {
+    message: "This field is required",
+  }),
+  sirName: z.string().min(2, {
+    message: "This field is required",
+  }),
+  email: z.string().min(2, {
+    message: "This field is required",
+  }),
+  phone: z.coerce.number().min(2, {
+    message: "This field is required",
+  }),
+});
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      sirName: "",
+      email: "",
+      phone: undefined,
+    },
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const { register, handleSubmit, formState: { errors } } = form;
+
+  const submit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="p-10 w-[50%]">
+      <h1>Registration Attendee Event</h1>
+      <Form {...form}>
+        <form onSubmit={handleSubmit(submit)}>
+          <FormItem className="my-5">
+            <FormLabel>First Name</FormLabel>
+            <Input {...register("firstName")} />
+            <FormMessage>
+              {errors.firstName && <span>{errors.firstName.message}</span>}
+            </FormMessage>
+          </FormItem>
+
+          <FormItem className="my-5">
+            <FormLabel>Sir Name</FormLabel>
+            <Input {...register("sirName")} />
+            <FormMessage>
+              {errors.sirName && <span>{errors.sirName.message}</span>}
+            </FormMessage>
+          </FormItem>
+
+          <FormItem className="my-5">
+            <FormLabel>Email</FormLabel>
+            <Input {...register("email")} />
+            <FormMessage>
+              {errors.email && <span>{errors.email.message}</span>}
+            </FormMessage>
+          </FormItem>
+
+          <FormItem className="my-5">
+            <FormLabel>Phone</FormLabel>
+            <Input type="number" {...register("phone")} />
+            <FormMessage>
+              {errors.phone && <span>{errors.phone.message}</span>}
+            </FormMessage>
+          </FormItem>
+
+          <Button type="submit" className="mt-6">
+            Submit
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
