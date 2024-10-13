d79d2109c4e5ac735e11d9cf48c1550079b33e0e
Zainab Ahmad <zainab.ahmad@stadim.be>
2024-10-13 11:56:04 +0300

    generate id for newAttendee

--- app/api/attendees/route.ts
+++ app/api/attendees/route.ts
@@ -1,22 +1,23 @@
-
-
-import  fs  from "fs";
+import fs from "fs";
 import data from "../../../lib/data.json";
 import { NextResponse } from "next/server";
 export async function GET() {
-  return NextResponse.json(
-    data,
-  );
+  return NextResponse.json(data);
 }
 
 export async function POST(req: Request) {
   try {
     const newAttendee = await req.json();
+
+    newAttendee.id = Math.floor(Math.random() * 100);
     data.push(newAttendee);
 
     fs.writeFileSync("lib/data.json", JSON.stringify(data, null, 2));
     return NextResponse.json(newAttendee, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: "Failed to add attendee" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to add attendee" },
+      { status: 500 }
+    );
   }
 }
