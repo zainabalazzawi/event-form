36381f351a74db8a1605c269958ee0cb12d5a936
Zainab Ahmad <zainab.ahmad@stadim.be>
2024-10-15 20:02:57 +0300

    commnet code

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -2,27 +2,28 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
 
-let users = []; // add type 
+
+// let users = []; // add type 
 export async function POST(req: Request) {
   try {
-    const { username, password, email } = await req.json();
+    // const { username, password, email } = await req.json();
 
-    const existingUser = users.find((user) => user.email === email);
-    if (existingUser) {
-      return NextResponse.json(
-        { error: "user already exists" },
-        { status: 409 }
-      );
-    }
+    // const existingUser = users.find((user) => user.email === email);
+    // if (existingUser) {
+    //   return NextResponse.json(
+    //     { error: "user already exists" },
+    //     { status: 409 }
+    //   );
+    // }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    // const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = {
-      username,
-      email,
-      password:hashedPassword,
-    };
-    users.push(newUser);
+    // const newUser = {
+    //   username,
+    //   email,
+    //   password:hashedPassword,
+    // };
+    // users.push(newUser);
 // use fs.writeFile to store 
     return NextResponse.json({ message: "user registered successfully" });
   } catch (error) {
