7f1672a6b640bc554fc8286f8658b4c002d2c50c
Zainab Ahmad <zainabalazzawi91@gmail.com>
2024-12-01 19:48:33 +0300

    clean up

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -9,16 +9,6 @@ const queryClient = new QueryClient();
 const client = new ApolloClient({
   uri: '/api/graphql',
   cache: new InMemoryCache(),
-  // defaultOptions: {
-  //   watchQuery: {
-  //     fetchPolicy: 'network-only',
-  //     errorPolicy: 'ignore',
-  //   },
-  //   query: {
-  //     fetchPolicy: 'network-only',
-  //     errorPolicy: 'all',
-  //   },
-  // },
 });
 
 export default function ClientLayout({
