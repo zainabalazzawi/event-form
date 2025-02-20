f086e105c783a57dcdc3044c345dc7248472bf29
Zainab Ahmad <zainabalazzawi91@gmail.com>
2025-02-20 11:09:53 +0300

    add pages

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivacyPolicy() {
+    return (
+      <div className="container mx-auto py-8">
+        <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
+      </div>
+    );
+  }
---
+++ app/terms/page.tsx
@@ -0,0 +1,7 @@
+export default function terms() {
+    return (
+      <div className="container mx-auto py-8">
+        <h1 className="text-2xl font-bold mb-4">terms</h1>
+      </div>
+    );
+  }
